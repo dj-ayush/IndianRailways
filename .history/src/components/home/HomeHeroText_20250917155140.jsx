740f4f53494ea00a1a7626581a527aff169e34f1
dj-ayush <ayushgupta.7ag@gmail.com>
2025-09-18 19:11:35 +0530

    remaining project code

---
+++ .history/src/components/home/HomeHeroText_20250917155140.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Video from './Video'
+
+const HomeHeroText = () => {
+    return (
+        <div className='font-[font1]  mt-102 lg:mt-0 pt-5 text-center'>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+                Railways 
+            </div>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
+                Dec
+                <div className='h-[7vw] w-[16vw] rounded-full -mt-3 overflow-hidden '>
+                    <Video />
+                </div>
+                luttered
+            </div>
+         
+        </div>
+    )
+}
+
+export default HomeHeroText
